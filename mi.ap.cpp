66463be947be204688c0d5e988e34b8463c0232a
Taiba Saleem <taibasaleemit@gmail.com>
2021-03-10 03:41:55 +0500

    second commit

---
+++ mi.ap.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"Enter integer value less than 10 = ";
+	cin>>i;
+	if(-1<i<=10)
+	{
+	
+	if(i==0)
+{
+
+	cout<<"A"<<endl;
+}
+if(i==1){
+
+	cout<<"B"<<endl;}
+if(i==2){
+
+	cout<<"C"<<endl;}
+if(i==3){
+
+	cout<<"D"<<endl;}
+if(i==4){
+
+	cout<<"E"<<endl;}
+if(i==5){
+
+	cout<<"F"<<endl;}
+if(i==6){
+
+	cout<<"G"<<endl;}
+if(i==7){
+
+	cout<<"H"<<endl;}
+if(i==8){
+
+	cout<<"I"<<endl;}
+if(i==9){
+
+	cout<<"J"<<endl;}
+if(i==10){
+
+	cout<<"K"<<endl;}
+}
+	
+	else
+	cout<<"Invalid Input";
+}
